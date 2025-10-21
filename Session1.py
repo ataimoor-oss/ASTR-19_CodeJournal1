56acac7874a0af87a5560fd38ec5e2288da687d7
ataimoor-oss <ataimoor@ucsc.edu>
2025-10-21 14:55:18 -0700

    Add files via upload

---
+++ Session1.py
@@ -0,0 +1,11 @@
+#Session1
+#Write a Python program to print out your full name, your preferred pronouns (optional), and two sentences about your favorite movie and your favorite food.
+
+def main():
+    print("Your Full Name: Ayeza Taimoor")
+    print("Your Preffered Prounouns: she/her")
+    print("Your Favorite Movie: Interstaller. Because I interned at NASA and I am in love with all things about Astronomy and Space")
+    print("Your Fave Food: Pani Puri. It's a desi food with a hard shell sort of like a crisp and it is filled with garbanzo beans, tomatoes, onion and spiced chaat.")
+
+if __name__ == "__main__":
+    main()
