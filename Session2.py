f01f06f8c7226c50fa2cfc09568388a93180b856
ataimoor-oss <ataimoor@ucsc.edu>
2025-10-21 15:01:58 -0700

    Add files via upload

---
+++ Session2.py
@@ -0,0 +1,20 @@
+#Session 2 
+#Write a Python that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.
+
+#Floating point sum
+a = 16.3
+b = 1.4
+sum_result = a + b
+print("Sum:", sum_result, "Type:", type(sum_result))
+
+#Integer Difference 
+x = 21
+y = 20
+diff_result = x - y
+print("Difference:", diff_result, "Type:", type(diff_result))
+
+#Float * Int product
+f = 6.7
+n = 420
+product_result = f * n
+print("Product:", product_result, "Type:", type(product_result))
